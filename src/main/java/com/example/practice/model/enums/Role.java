56a49de168d5d7aab1a8ccebd2aba28b69a4f62c
ay7ee <201213@astanait.edu.kz>
2022-06-23 13:40:16 +0600

    commit message

--- src/main/java/com/example/practice/model/enums/Role.java
+++
@@ -1,7 +0,0 @@
-package com.example.practice.model.enums;
-
-public enum Role {
-    SUPERADMIN,
-    ADMIN,
-    USER
-}
--- src/main/java/com/example/practice/registration/email/EmailSender.java
+++
@@ -1,5 +0,0 @@
-package com.example.practice.registration.email;
-
-public interface EmailSender {
-    void send(String to, String email);
-}
